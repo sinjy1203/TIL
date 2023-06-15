0cb004d92ef29079f1514b152fa9c2ec0e24f907
sinjy1203 <sinjy1203@gmail.com>
2023-06-16 02:57:21 +0900

    add computer_term & study restapi using django

---
+++ python-backend/RESTful-api/restapi_django_project/restapi_django_project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+from addresses import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('addresses/', views.address_list),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
